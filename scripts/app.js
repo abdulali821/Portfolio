e007a26cd71c474029e207097dead1b3e197eacc
abdulali821 <abdulali821@users.noreply.github.com>
2022-06-27 23:56:30 +0500

    comit

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,10 @@
-document.ready(function(){
-  $("#toggle").click(function(){
-    $("#toggleSlide").slideToggle();
-  });
+$(document).ready(function () {
+    $("#toggle").click(function () {
+        $("#toggleSlide").slideToggle();
+    });
+});
+$(document).ready(function () {
+    $("#toggleSlide a").click(function () {
+        $("#toggleSlide").slideToggle();
+    });
 });
